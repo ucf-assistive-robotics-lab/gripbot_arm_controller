00d7e0fb783fc537090c03951567bb72e40a1552
nick leone <test@example.com>
2019-08-05 14:43:41 -0400

    semi workin

--- src/app.py
+++ src/app.py
@@ -5,9 +5,14 @@ import rospy
 from gripbot_core_msgs.srv import SolvePositionIKRequest, SolvePositionIKResponse, SolvePositionIK
 from geometry_msgs.msg import PoseStamped, Pose
 from std_msgs.msg import Header
+from std_msgs.msg import String
+from std_msgs.msg import Float64
+
+joint_state_publishers = {}
+
 
 def position_kinematic(x, y, z):
-    rospy.wait_for_service('/gripbot/kinematics/arm/pos/IKService')
+    rospy.wait_for_service('/gripbot/kinematics/arm/IKService')
     try:
         ikrequest = SolvePositionIKRequest()
         initial_pose = Pose()
@@ -16,23 +21,45 @@ def position_kinematic(x, y, z):
         initial_pose.position.z = z
 
         ikrequest.pose_stamp = [PoseStamped()]
-        ikrequest.pose_stamp[0].header = Header(stamp=rospy.Time.now(), frame_id='base_mount_link')
+        ikrequest.pose_stamp[0].header = Header(
+            stamp=rospy.Time.now(), frame_id='base_mount_link')
 
         ikrequest.pose_stamp[0].pose = initial_pose
-        seviceHandle = rospy.ServiceProxy('/gripbot/kinematics/arm/pos/IKService', SolvePositionIK)
+        seviceHandle = rospy.ServiceProxy(
+            '/gripbot/kinematics/arm/IKService', SolvePositionIK)
         ikresponse = seviceHandle(ikrequest)
+    except Exception as e:
+        print("Application Failed because of something")
+        exit()
 
-        print(ikresponse)
-    except rospy.ServiceException as e:
-        print("Service call failed: %s".format(e))
+    if ikresponse.isValid[0] == True:
+        return ikresponse.joints
+    return None
 
-def usage():
-    return "%s [x y]"%sys.argv[0]
-  
+
+def publishJoint(jointName, position):
+    tname = jointName.split('_')
+    tname[len(tname)-1] = "controller"
+    jointName = "_".join(tname)
+
+    if jointName not in joint_state_publishers.keys():
+        topic = 'gripbot/{}/command'.format(jointName)
+        pub = rospy.Publisher(topic, Float64, queue_size=10)
+        joint_state_publishers[jointName] = pub
+
+    joint_state_publishers[jointName].publish(position)
+
+
 if __name__ == "__main__":
-    rospy.init_node("arm_position")
+    rospy.init_node("arm1_position")
     if len(sys.argv) == 4:
-        x = int(sys.argv[1])
-        y = int(sys.argv[2])
-        z = int(sys.argv[3])
-        position_kinematic(x, y, z)
+        x = float(sys.argv[1])
+        y = float(sys.argv[2])
+        z = float(sys.argv[3])
+        joint_state = position_kinematic(x, y, z)
+
+        for i in range(len(joint_state[0].name)):
+            jname = joint_state[0].name[i]
+            jpos = joint_state[0].position[i]
+
+            publishJoint(jname, jpos)
